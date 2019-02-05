cc2b004ae8169efcc38d8d6c8e4880cf88a64a1a
ErikVasquez <42862990+ErikVasquez@users.noreply.github.com>
2019-02-05 14:33:41 -0800

    Add files via upload

---
+++ Figuras Primitivas/Figuras Primitivas/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Figuras_Primitivas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Crear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Crear
+            // 
+            this.Crear.Location = new System.Drawing.Point(305, 340);
+            this.Crear.Name = "Crear";
+            this.Crear.Size = new System.Drawing.Size(75, 23);
+            this.Crear.TabIndex = 0;
+            this.Crear.Text = "Crear";
+            this.Crear.UseVisualStyleBackColor = true;
+            this.Crear.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Crear_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Crear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Crear;
+    }
+}
+
---
+++ Figuras Primitivas/Figuras Primitivas/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Primitivas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Crear_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics obj = CreateGraphics();
+            Pen pen = new Pen(Color.Black, 8);
+            obj.DrawLine(pen, 100, 10, 200, 200);
+            Graphics ob = this.CreateGraphics();
+            Pen pe = new Pen(Color.Crimson,8);
+            ob.DrawRectangle(pe, 300, 150, 100, 100);
+            Graphics o = this.CreateGraphics();
+            Pen p = new Pen(Color.DarkGoldenrod,8);
+            o.DrawEllipse(p, 550, 150, 100, 100);
+        }
+    }
+}
